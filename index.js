ad536e241118668381a54d95efb9d4c011254bf4
Mukesh Kumar <MukeshKumar@fico.com>
2018-09-20 14:53:42 +0530

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const Joi = require('joi');
 const app = express();
+const custom = require('./morgan-custom-function');
+const logger = require('./logger');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const config = require('config');
+console.log(`Application Name: ${config.get('name')}`);
+console.log(`Application Mail Server: ${config.get('mail.host')}`);
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(logger);
+if(app.get('env')==='development'){
+app.use(morgan(custom));
+app.use(helmet());
+console.log('Morgan Enabled.......');
+}
 const courses = [
-    {id: 1, name: 'C'},
-    {id: 2, name: 'C++'},
-    {id: 3, name: 'Java'}
+    {id: 1, name: 'C Programming Language'},
+    {id: 2, name: 'C++ Learning course'},
+    {id: 3, name: 'JavaEE Advanced'}
 ];
 app.get('/',(req, res)=>{
     res.send('Hello World!!!!');
@@ -15,16 +30,13 @@ app.get('/api/courses',(req,res)=>{
 });
 app.get('/api/courses/:id',(req,res)=>{
    const course = courses.find(c => c.id === +req.params.id);
-   if(!course) res.status(404).send(`The course with given id: ${req.params.id} is not available`);
+   if(!course) return res.status(404).send(`The course with given id: ${req.params.id} is not available`);
    res.send(course);
 });
 app.post('/api/courses', (req,res)=>{
     
    const {error} = validateCourse(req.body);
-    if(error){
-    res.status(400).send(error.details[0].message);
-    return;
-    }
+    if(error) return res.status(400).send(error.details[0].message);
     const course = {
         id: courses.length + 1,
         name: req.body.name
@@ -34,15 +46,20 @@ app.post('/api/courses', (req,res)=>{
 });
 app.put('/api/courses/:id',(req,res)=>{
     const course = courses.find(c => c.id === +req.params.id);
-    if(!course) res.status(404).send(`The course with given id: ${req.params.id} is not available`);
+    if(!course) return res.status(404).send(`The course with given id: ${req.params.id} is not available`);
     const {error} = validateCourse(req.body);
-    if(error){
-        res.status(400).send(error.details[0].message);
-        return;
-    }
+    if(error) return res.status(400).send(error.details[0].message);
     course.name = req.body.name;
-    courses.push(course);
-})
+    res.send(course);
+   // courses[+req.params.id - 1]=course;
+});
+app.delete('/api/courses/:id',(req,res)=>{
+    const course = courses.find(c => c.id === +req.params.id);
+    if(!course) return res.status(404).send(`The course with given id: ${req.params.id} is not available`);
+    const index = courses.indexOf(course);
+    courses.splice(index,1);
+    res.send(course);
+});
 function validateCourse(course) {
     const schema = {
         name: Joi.string().min(3).required()
---
+++ morgan-custom-function.js
@@ -0,0 +1,10 @@
+function customMorgan(tokens,req,res) {
+return [
+    tokens.method(req,res),' ',
+    tokens.url(req,res), ' ',
+    tokens.status(req,res),' ',
+    tokens.res(req,res,'content-length'), '-',
+    tokens['response-time'](req,res), 'ms'
+].join('')
+}
+module.exports =customMorgan
